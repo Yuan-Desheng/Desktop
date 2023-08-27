400e58b860818189a3d32d71ae0fca6ce07fa8a5
L <desheng.yuan@outlook.com>
2023-08-27 22:44:34 +0800

    6-1 vue-echarts组件库入门

--- imooc-datav-report-dev/src/main.js
+++ imooc-datav-report-dev/src/main.js
@@ -4,9 +4,11 @@ import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import * as ECharts from 'echarts';
+import VueEcharts from 'vue-echarts';
 
 Vue.config.productionTip = false
 Vue.prototype.$echarts = ECharts;
+Vue.component('v-chart', VueEcharts)
 Vue.use(ElementUI);
 
 new Vue({
