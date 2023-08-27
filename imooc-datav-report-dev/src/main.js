f6173b31f4ad66e82b4a5993a305e5e2f1d2068c
L <desheng.yuan@outlook.com>
2023-08-27 22:51:13 +0800

    6-2 v-charts组件库入门

--- imooc-datav-report-dev/src/main.js
+++ imooc-datav-report-dev/src/main.js
@@ -5,10 +5,12 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import * as ECharts from 'echarts';
 import VueEcharts from 'vue-echarts';
+import VeLine from 'v-charts/lib/line.common'
 
 Vue.config.productionTip = false
 Vue.prototype.$echarts = ECharts;
 Vue.component('v-chart', VueEcharts)
+Vue.component(VeLine.name, VeLine)
 Vue.use(ElementUI);
 
 new Vue({
