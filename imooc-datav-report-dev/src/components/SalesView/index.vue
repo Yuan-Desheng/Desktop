849f279f239ddb38c4684661c683391ea4211543
yuandesheng <18603542180@163.com>
2023-08-28 15:16:34 +0800

    6-5 销售趋势组件容器+头部开发

--- imooc-datav-report-dev/src/components/SalesView/index.vue
+++ imooc-datav-report-dev/src/components/SalesView/index.vue
@@ -1,58 +1,128 @@
 <template>
-  <v-chart class="echarts" :option="option" />
+  <div class="sale-view">
+    <el-card shadow="hover">
+      <template v-slot:header>
+        <div class="menu-wrapper">
+          <el-menu
+            :default-active="activeIndex"
+            mode="horizontal"
+            @select="handleSelect"
+            class="sale-view-menu"
+          >
+            <el-menu-item index="1">销售额</el-menu-item>
+            <el-menu-item index="2">访问量</el-menu-item>
+          </el-menu>
+          <div class="menu-right">
+            <el-radio-group v-model="radio">
+              <el-radio-button label="今日"></el-radio-button>
+              <el-radio-button label="本周"></el-radio-button>
+              <el-radio-button label="本月"></el-radio-button>
+              <el-radio-button label="今年"></el-radio-button>
+            </el-radio-group>
+            <el-date-picker
+              v-model="date"
+              type="daterange"
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              size="small"
+              unlink-panels
+              :picker-options="pickerOptions"
+              class="sale-view-date-picker"
+            />
+          </div>
+        </div>
+      </template>
+      <template>
+
+      </template>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      option: {
-        title: {
-          text: "Traffic Sources",
-          left: "center"
-        },
-        tooltip: {
-          trigger: "item",
-          formatter: "{a} <br/>{b} : {c} ({d}%)"
-        },
-        legend: {
-          orient: "vertical",
-          left: "left",
-          data: [
-            "Direct",
-            "Email",
-            "Ad Networks",
-            "Video Ads",
-            "Search Engines"
-          ]
-        },
-        series: [
+      activeIndex: '1',
+      radio: '今日',
+      date: null,
+      pickerOptions: {
+        shortcuts: [
           {
-            name: "Traffic Sources",
-            type: "pie",
-            radius: "55%",
-            center: ["50%", "60%"],
-            data: [
-              { value: 335, name: "Direct" },
-              { value: 310, name: "Email" },
-              { value: 234, name: "Ad Networks" },
-              { value: 135, name: "Video Ads" },
-              { value: 1548, name: "Search Engines" }
-            ],
-            emphasis: {
-              itemStyle: {
-                shadowBlur: 10,
-                shadowOffsetX: 0,
-                shadowColor: "rgba(0, 0, 0, 0.5)"
-              }
+            text: '最近一周',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', [start, end]);
+            }
+          },
+          {
+            text: '最近一个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+              picker.$emit('pick', [start, end]);
+            }
+          },
+          {
+            text: '最近三个月',
+            onClick(picker) {
+              const end = new Date();
+              const start = new Date();
+              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+              picker.$emit('pick', [start, end]);
             }
           }
         ]
       }
-    };
+    }
+  },
+  methods: {
+    handleSelect(index) {
+      this.activeIndex = index;
+      console.log(this.activeIndex);
+
+    }
   }
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.sale-view{
+  margin-top: 20px;
+
+  .menu-wrapper {
+    position: relative;
+    display: flex;
+    .sale-view-menu {
+      width: 100%;
+      padding-left: 20px;
+      .el-menu-item {
+        height: 50px;
+        line-height: 50px;
+        margin: 0 20px;
+      }
+    }
+    .menu-right {
+      position: absolute;
+      top: 0;
+      right: 20px;
+      height: 50px;
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      .sale-view-date-picker {
+        margin-left: 20px;
+      }
+    }
+  }
+}
+
+::v-deep .el-card__header {
+  border-bottom: none;
+  padding: 0;
+}
 </style>
